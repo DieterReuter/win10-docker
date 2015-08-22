c74647f68a0ba30a31ede27f0704a13156ea2756
Dieter Reuter <dieter.reuter@me.com>
2015-08-23 00:52:15 +0200

    Install Chrome, Docker, Docker-Engine and Virtualbox

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -3,3 +3,17 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 
 # install git + ssh
 choco install -y git -params "/GitAndUnixToolsOnPath"
+
+# instal Chrome browser
+choco install -y GoogleChrome
+
+# install docker tools
+choco install -y docker -version 1.8.1
+choco install -y docker-machine -version 0.4.1
+
+# enable UAC
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1
+
+# install Oracle Virtualbox
+choco install -y virtualbox
+# choco install -y vmwareworkstation
